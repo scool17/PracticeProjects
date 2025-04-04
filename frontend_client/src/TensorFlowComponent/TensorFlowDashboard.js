d4f287d6ce28cd37cce56edfebdc78191239fa11
Shubham Goenka <shubham.17101997.goenka@gmail.com>
2025-04-04 23:41:41 +0530

    Adding plots in react

--- frontend_client/src/TensorFlowComponent/TensorFlowDashboard.js
+++ frontend_client/src/TensorFlowComponent/TensorFlowDashboard.js
@@ -3,23 +3,68 @@ import { useState, useEffect } from "react";
 
 export default function TensorFlowDashboard() {
 
-    const [imageUrl, setImageUrl] = useState("");
+    const [dashboardProps, setDashboardProps] = useState({
+      healthifyHeatmap: "",
+      healthifyLoss: "",
+      healthifyAccuracy: "",
+      healthifySequentialModelSummary: "",
+      irisFunctionalModelSummary: "",
+    });
 
     useEffect(() => {
-      setImageUrl("/tensorflow/correlation_heatmap"); // Flask API endpoint
-    }, []);
+      setDashboardProps(prevProps => ({
+        ...prevProps,
+        healthifyHeatmap: "/tensorflow/healthify_heatmap",
+        healthifyLoss: "/tensorflow/healthify_loss",
+        healthifyAccuracy: "/tensorflow/healthify_accuracy",
+      }));
+
+      fetch("/tensorflow/healthify_sequential_model_summary")
+      .then((res) => res.text())
+      .then((data) => setDashboardProps(prevProps => ({...prevProps, healthifySequentialModelSummary: data})))
+      .catch((err) => console.error("Error loading summary:", err));
+
+      fetch("/tensorflow/iris_functional_model_summary")
+      .then((res) => res.text())
+      .then((data) => setDashboardProps(prevProps => ({...prevProps, irisFunctionalModelSummary: data})))
+      .catch((err) => console.error("Error loading summary:", err));
+  }, []);
 
     return (
         <div>
             <h2 > Welcome to the TensorFlow Dashboard</h2>
             <div>
                 <h4 style={{margin: 0}}> Correlation Heatmap </h4>
-                {imageUrl ? (
-        <img src={imageUrl} alt="Correlation Heatmap" style={{ width: "100%", maxWidth: "600px" }} />
+                {dashboardProps.healthifyHeatmap ? (
+        <img src={dashboardProps.healthifyHeatmap} alt="Correlation Heatmap" style={{ width: "100%", maxWidth: "600px" }} />
       ) : (
         <p>Loading...</p>
       )}
             </div>
+            <div>
+                {/* <h4 style={{margin: 0}}> Correlation Heatmap </h4> */}
+                {dashboardProps.healthifyLoss ? (
+        <img src={dashboardProps.healthifyLoss} alt="Loss" style={{ width: "100%", maxWidth: "600px" }} />
+      ) : (
+        <p>Loading...</p>
+      )}
+            </div>
+            <div>
+                {/* <h4 style={{margin: 0}}> Correlation Heatmap </h4> */}
+                {dashboardProps.healthifyAccuracy ? (
+        <img src={dashboardProps.healthifyAccuracy} alt="Accuracy" style={{ width: "100%", maxWidth: "600px" }} />
+      ) : (
+        <p>Loading...</p>
+      )}
+            </div>
+            <div
+      className="html-content"
+      dangerouslySetInnerHTML={{ __html: dashboardProps.healthifySequentialModelSummary }}
+    />
+            <div
+      className="html-content"
+      dangerouslySetInnerHTML={{ __html: dashboardProps.irisFunctionalModelSummary }}
+    />
         </div>
     )
 }
